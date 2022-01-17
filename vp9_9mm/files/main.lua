488826470ed26fa44926cc2bba8c69cbef313cc8
Roland Wurzer <roidal@googlemail.com>
2022-01-17 11:31:25 +0100

    vp9_9mm cleanup.

--- vp9_9mm/files/main.lua
+++ vp9_9mm/files/main.lua
@@ -3,21 +3,14 @@ if not game.modSupport or not game.modSupport.weaponMods then
 	return
 end
 
-function copy(o)
-	local n = {}
-	for k, v in pairs(o) do	
-		n[k] = v
-	end
-	return n
-end
-
-local weapon = copy(weapons:getData("vp9"))
+local weapon = {}
+vp9 = weapons:getData("vp9")
 
 weapon.id = "vp9_9"
 weapon.name = _T("WEAPON_NAME_VP9_9", "B&T VP9 9x19mm")
 weapon.trivia = _T("WEAPON_NAME_VP9_9_TRIVIA", "The 9x19mm Version of the VP9. Using more common ammunition but being 20% louder.")
 weapon.ammoType = "9x19MM"
-weapon.noiseRadius = weapon.noiseRadius * 1.2
+weapon.noiseRadius = vp9.noiseRadius * 1.2
 
-weapons:register(weapon)
-local campaignData = game.getCampaignData("intravenous"):addLoadoutWeapon(weapons.TYPES.SECONDARY, "vp9_9")
+weapons:register(weapon, "vp9")
+game.getCampaignData("intravenous"):addLoadoutWeapon(weapons.TYPES.SECONDARY, "vp9_9")
